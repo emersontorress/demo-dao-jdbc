1c90269a481e566a31005e72218e8d9bcd7e62bc
Emerson Torres <emsn.rj@gmail.com>
2024-12-10 19:04:19 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Departament;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,7 +11,9 @@ public class Program {
 
 		Departament obj = new Departament(1, "books");
 		
-		System.out.println(obj);
+		Seller seller = new Seller(21, "Bob", "bobo@gmail.com", new Date(), 3000.00, obj);
+		
+		System.out.println(seller);
 	}
 
 }
